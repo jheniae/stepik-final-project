6030eed155e45738dc3d429e81ec42bb474c833e
jheniae <jheniae@gmail.com>
2019-09-15 13:08:44 +0300

    add check the elements on the page

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # initialize the Page Object, pass the driver instance and url to the constructor
     page.open()  # open the page
     page.go_to_login_page()  # execute the page method - go to the login page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
